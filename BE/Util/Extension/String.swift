f4caeb7834b97b9375f7cd2128c97bc91b813224
Luke_Kong <kongthn@gmail.com>
2022-07-02 21:28:24 +0900

    [Feat] String 익스텐션 추가 - inserComma

--- BE/Util/Extension/String.swift
+++ BE/Util/Extension/String.swift
@@ -17,4 +17,41 @@ extension String {
         let end = index(start, offsetBy: min(to, self.count) - from)
         return String(self[start ..< end])
     }
+    
+    // MARK: comma
+    // ex. "1234567890".insertComma == "1,234,567,890"
+    var insertComma: String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        if let _ = self.range(of: ".") {
+            let numberArray = self.components(separatedBy: ".")
+            if numberArray.count == 1 {
+                var numberString = numberArray[0]
+                if numberString.isEmpty {
+                    numberString = "0"
+                }
+                guard let doubleValue = Double(numberString)
+                    else { return self }
+                return numberFormatter.string(from: NSNumber(value: doubleValue)) ?? self
+            } else if numberArray.count == 2 {
+                var numberString = numberArray[0]
+                if numberString.isEmpty {
+                    numberString = "0"
+                }
+                guard let doubleValue = Double(numberString)
+                    else {
+                        return self
+                }
+                return (numberFormatter.string(from: NSNumber(value: doubleValue)) ?? numberString) + ".\(numberArray[1])"
+            }
+        }
+        else {
+            guard let doubleValue = Double(self)
+                else {
+                    return self
+            }
+            return numberFormatter.string(from: NSNumber(value: doubleValue)) ?? self
+        }
+        return self
+    }
 }
