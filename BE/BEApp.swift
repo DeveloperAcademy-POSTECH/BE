7dfc2641a951020717ac25d23733616e75ed3dd8
Luke_Kong <kongthn@gmail.com>
2022-07-02 14:48:53 +0900

    Initial Commit

---
+++ BE/BEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEApp.swift
+//  BE
+//
+//  Created by GOngTAE on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct BEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
