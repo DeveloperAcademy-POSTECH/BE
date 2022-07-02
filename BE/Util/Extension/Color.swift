95ef27dbe5e5dde60b8be5b2cb20183e5959a701
Luke_Kong <kongthn@gmail.com>
2022-07-02 18:49:28 +0900

    [Add] 컬러 에셋, 익스텐션

---
+++ BE/Util/Extension/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  BE
+//
+//  Created by GOngTAE on 2022/07/02.
+//
+
+import SwiftUI
+
+extension Color {
+    static let main = Color("mainColor")
+    static let background = Color("backgroundColor")
+    static let container = Color("containerColor")
+}
