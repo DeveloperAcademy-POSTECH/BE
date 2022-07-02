7457d029b7875b7c922023e0a4d9a976d5b8efff
phainestha1 <250wons@gmail.com>
2022-07-03 02:35:10 +0900

    [Add] Reusable Menutitle

---
+++ BE/Source/Components/MenuTitle.swift
@@ -0,0 +1,22 @@
+//
+//  MenuTitle.swift
+//  BE
+//
+//  Created by Noah's Ark on 2022/07/03.
+//
+
+import SwiftUI
+
+struct MenuTitle: View {
+    var body: some View {
+        Text("MENU")
+            .foregroundColor(Color.main)
+            .padding(.top, 11)
+    }
+}
+
+struct MenuTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuTitle()
+    }
+}
