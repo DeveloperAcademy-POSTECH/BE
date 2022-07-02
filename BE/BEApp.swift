d03d701697acff5f306a001ad25da6030d12c691
Luke_Kong <kongthn@gmail.com>
2022-07-02 15:06:05 +0900

    [Add] AppDelegate - firebase configure

--- BE/BEApp.swift
+++ BE/BEApp.swift
@@ -5,13 +5,25 @@
 //  Created by GOngTAE on 2022/07/02.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct BEApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
