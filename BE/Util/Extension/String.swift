ab914f8c35861fd5c0e978aeda4465840952370a
Luke_Kong <kongthn@gmail.com>
2022-07-02 21:27:31 +0900

    [Feat] substring 메서드 익스텐션 구현

--- BE/Util/Extension/String.swift
+++ BE/Util/Extension/String.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 extension String {
-    
+    // MARK: substring
+    func substring(from: Int, to: Int) -> String {
+        guard (to >= 0) && (from <= self.count) && (from <= to) else {
+            return ""
+        }
+        let start = index(startIndex, offsetBy: max(from, 0))
+        let end = index(start, offsetBy: min(to, self.count) - from)
+        return String(self[start ..< end])
+    }
 }
