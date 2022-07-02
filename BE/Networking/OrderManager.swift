ad038c34f8a531dadd72d13fd3e6ac2856cc1d36
Luke_Kong <kongthn@gmail.com>
2022-07-03 05:09:00 +0900

    [Add] 주문 네트워크 로직 연결

--- BE/Networking/OrderManager.swift
+++ BE/Networking/OrderManager.swift
@@ -6,3 +6,105 @@
 //
 
 import Foundation
+import Alamofire
+
+class OrderManager: ObservableObject {
+    static let shared = OrderManager()
+    private init() {}
+    
+    func requestPickUpUsers(completion: @escaping ([User], Error?) -> Void) {
+        AF.request(Secret.orderUrl)
+            .responseString { response in
+                switch response.result {
+                case .success(let string):
+                    print(string)
+                    var jsonString = string.replacingOccurrences(of: "\'", with: "\"")
+                    jsonString.removeFirst()
+                    jsonString.removeLast()
+                    print(jsonString)
+                    let jsonData = jsonString.data(using: .utf8)!
+                    do {
+                        let decoder = JSONDecoder()
+                        let tableData = try decoder.decode([User].self, from: jsonData)
+                        completion(tableData, nil)
+                    }
+                    catch {
+                        print (error)
+                        completion([], error)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+    func order(with data: OrderData) {
+        let parameters: [[String: String]] = [[
+            "user" : data.user,
+            "menu" : data.menu,
+            "lastPhoneNumber" : data.lastPhoneNumber
+        ]]
+        
+        let url = URL(string: Secret.orderUrl)
+        var request = URLRequest(url: url!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        AF.request(request)
+            .responseString { response in
+                
+                let jsonData = response.request?.httpBody
+                
+                do {
+                    let decoder = JSONDecoder()
+                    let tableData = try decoder.decode([OrderData].self, from: jsonData!)
+                    print(tableData)
+                }
+                catch {
+                    print(error)
+                }
+                
+                switch response.result {
+                case .success(let string):
+                    print(string)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func cancellOrder(with userName: String) {
+        let parameters: [[String: String]] = [[
+            "user" : userName
+        ]]
+        
+        let url = URL(string: Secret.orderUrl)
+        var request = URLRequest(url: url!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "PATCH"
+        request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        AF.request(request)
+            .responseString { response in
+                switch response.result {
+                case .success(let string):
+                    print(string)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+    }
+}
+
+struct OrderData: Codable {
+    let user: String
+    var menu: String
+    let lastPhoneNumber: String
+}
+
+struct Response: Codable {
+    var user: [User]
+}
+
+struct User: Codable {
+    var user: String
+}
--- BE/Source/ContentView.swift
+++ BE/Source/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var orderManager = OrderManager.shared
     // OnBoarding
     @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
     
@@ -16,6 +17,9 @@ struct ContentView: View {
             .padding()
             .fullScreenCover(isPresented: $isFirstLaunching) {
                 OnboardingTabView(isFirstLaunching: $isFirstLaunching)
+            }
+            .onAppear {
+                orderManager.order(with: OrderData(user: "A.Theo", menu: "간장닭갈비덮밥", lastPhoneNumber: "01098734972"))
             }
     }
 }
