853f48741f56d215ef503e60c25735460b258340
E-know <eknow25@gmail.com>
2022-07-06 21:44:41 +0900

    [Fix] OnboardingView 글자 깨짐
    Source - Components - OnbardingTabView
    글자 깨짐 관련 이미지 및 버튼 패딩 조정

--- BE/Source/OnboardingTabView.swift
+++ BE/Source/OnboardingTabView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct OnboardingTabView: View {
     @Binding var isFirstLaunching: Bool
-    
+
     var body: some View {
         NavigationView {
             ZStack {
                 Color.main.ignoresSafeArea()
-                
+
                 VStack {
                     TabView {
                         Group {
@@ -22,25 +22,25 @@ struct OnboardingTabView: View {
                             OnboardingSecondView()
                             OnboardingLastView(isFirstLaunching: $isFirstLaunching)
                         }
-                        .padding(.top, 300)
+                        .padding(.top, UIScreen.main.bounds.height >= 812 ? 300 : 150)
                     }
-                    .tabViewStyle(PageTabViewStyle())
-                    .navigationBarHidden(true)
-                    .navigationTitle("")
+                        .tabViewStyle(PageTabViewStyle())
+                        .navigationBarHidden(true)
+                        .navigationTitle("")
                 }
-                .overlay(
+                    .overlay(
                     VStack {
                         HStack {
                             Spacer()
-                            
+
                             Image("azeomi")
                                 .resizable()
-                                .frame(width: 350, height: 350)
-                            
+                                .resizeLogo()
+
                             Spacer()
                         }
-                        .allowsHitTesting(false)
-                        
+                            .allowsHitTesting(false)
+
                         Spacer()
                     }
                 )// overlay
@@ -51,10 +51,30 @@ struct OnboardingTabView: View {
 }// OnboardingTabView
 
 struct OnboardingTabView_Previews: PreviewProvider {
-    
+
     @State static var isFirstLaunching = true
-    
+
     static var previews: some View {
         OnboardingTabView(isFirstLaunching: $isFirstLaunching)
+            .previewDevice("iPod touch (7th generation)")
+    }
+}
+
+
+extension Image {
+    func resizeLogo() -> some View {
+        let height = UIScreen.main.bounds.height
+
+        switch Int(height) {
+        case ...812:
+            return self.resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 350 / 844 * height)
+
+        default:
+            return self.resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 350)
+        }
     }
 }
